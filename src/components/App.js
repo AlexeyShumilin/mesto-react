80909fd3bb02d4373c915204d62a97e7600963e0
Alexey Shumilin <shumil.alexey@yandex.ru>
2020-11-10 19:53:38 +0200

    first rewiew

--- src/components/App.js
+++ src/components/App.js
@@ -1,146 +1,150 @@
-import React from 'react';
-import '../index.css';
-import Main from "./Main";
-import Footer from "./Footer";
+import React from "react";
+import "../index.css";
 import Header from "./Header";
-import PopupWithForm from './PopupWithForm';
-import ImagePopup from './ImagePopup';
-
+import Main from "./Main";
+import PopupWithForm from "./PopupWithForm";
+import ImagePopup from "./ImagePopup";
+import Footer from "./Footer";
 
 function App() {
+  //стейты попапов
+  const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(
+    false
+  );
+  const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false);
+  const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false);
+  const [isConfirmPopupOpen, setConfirmPopupOpen] = React.useState(false);
+  const [isImagePopupOpen, setIsImagePopupOpen] = React.useState(false);
+  const [selectedCard, setSelectedCard] = React.useState(null);
 
-    //стейты попапов
-    const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(false);
-    const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false);
-    const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false);
-    const [isConfirmPopupOpen, setConfirmPopupOpen] = React.useState(false);
-    const [isImagePopupOpen, setIsImagePopupOpen] = React.useState(false);
-    const [selectedCard, setSelectedCard] = React.useState(null);
+  //setstate block
 
-//setstate block
+  const handleEditProfileClick = function () {
+    setEditProfilePopupOpen(true);
+  };
 
-    const handleEditProfileClick = function () {
-        setEditProfilePopupOpen(true);
-    };
+  const handleAddPlaceClick = function () {
+    setAddPlacePopupOpen(true);
+  };
 
-    const handleAddPlaceClick = function () {
-        setAddPlacePopupOpen(true);
-    };
+  const handleEditAvatarClick = function () {
+    setEditAvatarPopupOpen(true);
+  };
 
-    const handleEditAvatarClick = function () {
-        setEditAvatarPopupOpen(true);
-    };
+  const closeAllPopups = function () {
+    setEditProfilePopupOpen(false);
+    setAddPlacePopupOpen(false);
+    setIsImagePopupOpen(false);
+    setEditAvatarPopupOpen(false);
+    setConfirmPopupOpen(false);
+    setSelectedCard(null);
+  };
 
-    const closeAllPopups = function () {
-        setEditProfilePopupOpen(false);
-        setAddPlacePopupOpen(false);
-        setEditAvatarPopupOpen(false);
-        setConfirmPopupOpen(false);
-        setIsImagePopupOpen(false);
-        setSelectedCard(null);
-    };
+  const handleCardClick = function (card) {
+    setIsImagePopupOpen(true);
+    setSelectedCard(card);
+  };
 
-    const handleCardClick = function (card) {
-        setIsImagePopupOpen(true);
-        setSelectedCard(card);
-    };
-
-    return (
-        <div className="page">
-            <Header/>
-            <Main
-                onEditProfile={handleEditProfileClick}
-                onAddPlace={handleAddPlaceClick}
-                onEditAvatar={handleEditAvatarClick}
-                onCardClick={handleCardClick}
+  return (
+    <div className="page">
+      <Header />
+      <Main
+        onEditProfile={handleEditProfileClick}
+        onAddPlace={handleAddPlaceClick}
+        onEditAvatar={handleEditAvatarClick}
+        onCardClick={handleCardClick}
+      />
+      <Footer />
+      <PopupWithForm
+        title={"Редактировать профиль"}
+        name={"profile"}
+        children={
+          <>
+            <input
+              id="author-input"
+              className="popup__item"
+              type="text"
+              required
+              placeholder="Введите имя"
+              name="name"
+              minLength={"2"}
+              maxLength={"40"}
             />
-            <Footer/>
-            <PopupWithForm
-                title={'Редактировать профиль'}
-                name={'profile'}
-                children={
-                    <>
-                        <input
-                            id="author-input"
-                            className="popup__item"
-                            type="text"
-                            required
-                            placeholder="Введите имя"
-                            name="name"
-                            minLength={"2"}
-                            maxLength={"40"}/>
-                        <input
-                            id="about-input"
-                            className="popup__item"
-                            type="text"
-                            required
-                            placeholder="Расскажите о себе"
-                            name="about"
-                            minLength={"2"}
-                            maxLength={"200"}/>
-                    </>
-                }
-                isOpen={isEditProfilePopupOpen}
-                onClose={closeAllPopups}
-                textOnButton={'Сохранить'}
+            <input
+              id="about-input"
+              className="popup__item"
+              type="text"
+              required
+              placeholder="Расскажите о себе"
+              name="about"
+              minLength={"2"}
+              maxLength={"200"}
             />
-            <PopupWithForm
-                title={'Новое место'}
-                name={'add-card'}
-                children={
-                    <>
-                        <input
-                            id="name-input"
-                            className="popup__item"
-                            type="text"
-                            required
-                            placeholder="Название"
-                            name="name"
-                            minLength={"1"}
-                            maxLength={"30"}/>
-                        <input
-                            id="link-input"
-                            className="popup__item"
-                            type="url"
-                            required
-                            placeholder="Ссылка на картинку"
-                            name="link"/>
-                    </>
-                }
-                isOpen={isAddPlacePopupOpen}
-                onClose={closeAllPopups}
-                textOnButton={'Создать'}
+          </>
+        }
+        isOpen={isEditProfilePopupOpen}
+        onClose={closeAllPopups}
+        textOnButton={"Сохранить"}
+      />
+      <PopupWithForm
+        title={"Новое место"}
+        name={"add-card"}
+        children={
+          <>
+            <input
+              id="name-input"
+              className="popup__item"
+              type="text"
+              required
+              placeholder="Название"
+              name="name"
+              minLength={"1"}
+              maxLength={"30"}
             />
-            <PopupWithForm
-                title={'Обновить аватар'}
-                name={'update-avatar'}
-                children={
-                    <input
-                        id="avatar-input"
-                        className="popup__item"
-                        type="url"
-                        placeholder="ссылка на аватар"
-                        name="avatar"/>
-                }
-                isOpen={isEditAvatarPopupOpen}
-                onClose={closeAllPopups}
-                textOnButton={'Сохранить'}
+            <input
+              id="link-input"
+              className="popup__item"
+              type="url"
+              required
+              placeholder="Ссылка на картинку"
+              name="link"
             />
-            <PopupWithForm
-                title={'Вы уверены'}
-                name={'confirm'}
-                isOpen={isConfirmPopupOpen}
-                onClose={closeAllPopups}
-                textOnButton={'Да'}
-            />
-            <ImagePopup
-                card={selectedCard}
-                isOpen={isImagePopupOpen}
-                onClose={closeAllPopups}
-            />
-        </div>
-    );
+          </>
+        }
+        isOpen={isAddPlacePopupOpen}
+        onClose={closeAllPopups}
+        textOnButton={"Создать"}
+      />
+      <PopupWithForm
+        title={"Обновить аватар"}
+        name={"update-avatar"}
+        children={
+          <input
+            id="avatar-input"
+            className="popup__item"
+            type="url"
+            placeholder="ссылка на аватар"
+            name="avatar"
+          />
+        }
+        isOpen={isEditAvatarPopupOpen}
+        onClose={closeAllPopups}
+        textOnButton={"Сохранить"}
+      />
+      <ImagePopup
+        card={selectedCard}
+        isOpen={isImagePopupOpen}
+        onClose={closeAllPopups}
+      />
+      <PopupWithForm
+        title={"Вы уверены"}
+        name={"confirm"}
+        isOpen={isConfirmPopupOpen}
+        onClose={closeAllPopups}
+        textOnButton={"Да"}
+      />
+    </div>
+  );
 }
-
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import mestologo from '../images/logo.svg'
+import React from "react";
+import mestologo from "../images/logo.svg";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <img alt="логотип Место" className="header__logo" src={mestologo}/>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <img alt="логотип Место" className="header__logo" src={mestologo} />
+    </header>
+  );
+};
 
 export default Header;
